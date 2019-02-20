e441fef6026eccadaffcfb3a7483bc64f41c6c9a
Guccif3r <kukarachakuku228@gmail.com>
2019-02-20 21:37:19 +0300

    Little features and some new bugs. Corrected checking, that target in a tube. Changed names of axis. Found bugs with centers of access circles.

--- board/Unica_20I9_STM32F405_FC/src/predictor_module.c
+++ board/Unica_20I9_STM32F405_FC/src/predictor_module.c
@@ -9,24 +9,23 @@ stateGPS_t stateGPS;
 stateIMU_isc_t stateIMU;
 //WTF
 
-bool check_tube_target(float a, float b, float c) {
-	float dist = abs(c) / sqrt(a * a + b * b);
-	if (dist < tube_size) {
-		return 1;
+bool check_tube_target(double a, double b, double c, double z, double x, bool flag) {
+	double dist = abs(c) / sqrt(a * a + b * b);
+	double target_dist = sqrt(z * z + x * x);
+	if ((asin(dist / target_dist) < 0.17) && (flag)) {
+		return true;
 	}
-	return 0;
+	return false;
 }
-void height_predictor() {
-	float x = stateGPS.coordinates[0];
-	float y = stateGPS.coordinates[1];
-	float z = stateGPS.coordinates[2];
-	float target_dist = sqrt(x * x + y * y);
-	z -= delta_h;
-	float distmin, distmax;
-	distmin = z / tan(descent_angle_max);
-	distmax = z / tan(descent_angle_min);
+void height_predictor(double x, double y, double z) {
+	double target_dist = sqrt(z * z + x * x);
+	y -= delta_h;
+	double distmin, distmax;
+	distmin = y / tan(descent_angle_max);
+	distmax = y / tan(descent_angle_min);
 	if ((target_dist > distmin) && (target_dist < distmax)) {
-		straight_flight(atan2(z, target_dist));
+		double good_angle = atan2(y, target_dist);
+		straight_flight(atan2(y, target_dist));
 		return;
 	}
 	if (target_dist <= distmin) {
@@ -42,28 +41,29 @@ void height_predictor() {
 
 void direction_predictor() {
 	char turn;
-	float x = stateGPS.coordinates[0];
-	float y = stateGPS.coordinates[1];
-	//float z = stateGPS.coordinates[2];
-	float vx = stateIMU.velocities[0];
-	float vy = stateIMU.velocities[1];
-	//float vz = stateIMU.velocities[2];
-	float x0 = x + vx * spare_time;
-	float y0 = y + vy * spare_time;
-	float a, b, c;
-	a = y0 - y;
-	b = x - x0;
-	c = x0 * y - x * y0;
-	if (check_tube_target(a, b, c)) {
-		height_predictor();
+	double x, y, z;
+	double vz, vx;
+	double z0 = z + vz * spare_time;
+	double x0 = x + vx * spare_time;
+	double a, b, c;
+	a = x0 - x;
+	b = z - z0;
+	c = z0 * x - z * x0;
+	bool flag = 1;
+	if (sqrt(z0 * z0 + x0 * x0) > sqrt(z * z + x * x)) {
+		flag = 0;
 	}
-	float centerX, centerY;
-	float r = radius_of_turn_circle;
-	centerX = - a * r;
-	centerY = b * r;
-	float target_lenght;
-	target_lenght = sqrt(centerX * centerX + centerY * centerY);
-	float access_radius;
+	if (check_tube_target(a, b, c, z, x, flag)) {
+		height_predictor(x, y, z);
+		return;
+	}
+	double centerz, centerx;
+	double r = radius_of_turn_circle;
+	centerz = - a * r;
+	centerx = b * r;
+	double target_lenght;
+	target_lenght = sqrt(centerz * centerz + centerx * centerx);
+	double access_radius;
 	access_radius = r + delta_r;
 	if (c > 0) {
 		turn = 'L';
@@ -71,27 +71,27 @@ void direction_predictor() {
 		turn = 'R';
 	}
 	if (target_lenght >= access_radius) {
-		turn_flight(x0, y0, turn);
+		turn_flight(z0, x0, turn);
 		return;
 	} else {
-		calculating_distance_of_linear_further_motion(access_radius, r, vx, vy, turn);
+		calculating_distance_of_linear_further_motion(access_radius, r, vz, vx, turn);
 		return;
 	}
 	return;
 }
 
-void calculating_distance_of_linear_further_motion(float R, float r, float vx, float vy, char turn) {
-	float dist;
-	float coordinates[3] = {0};
-	float new_coordinates[3] = {0};
-	float quat[4] = {0};
+void calculating_distance_of_linear_further_motion(double R, double r, double vz, double vx, char turn) {
+	double dist;
+	double coordinates[3] = {0};
+	double new_coordinates[3] = {0};
+	double quat[4] = {0};
 	quat_invert(stateIMU_isc.quaternion, quat);
 	vect_rotate(stateGPS.coordinates, quat, coordinates);
-	float x = coordinates[0];
-	float y = coordinates[1];
-	float z = coordinates[2];
+	double z = coordinates[0];
+	double x = coordinates[1];
+	double z = coordinates[2];
 	dist = sqrt(R * R - (z - r) * (z - r));
-	x += dist + vx * spare_time;
+	z += dist + vz * spare_time;
 	vect_rotate(coordinates, stateIMU_isc.quaternion, new_coordinates);
 	turn_flight(new_coordinates[0], new_coordinates[1], turn);
     return;
